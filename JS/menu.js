433fc81fc6f2449d41ec043dc3ebf62880c6ed9c
Paul Antonio Flores Castro <56978561+PaulAFC14@users.noreply.github.com>
2021-08-02 21:59:33 -0600

    Add files via upload

---
+++ JS/menu.js
@@ -0,0 +1,8 @@
+$(function(){
+	/*var header = document.getElementById('header');
+	var headroom = new Headroom(header);
+	headroom.init();*/
+	var banner = document.getElementById('ban');
+	var badroom = new Headroom(banner);
+	badroom.init();
+});
